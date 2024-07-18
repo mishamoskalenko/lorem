429968a998373addd88423494b1c8acd85e76c59
Misha <misamoskalenko318@gmail.com>
2024-07-18 14:11:38 +0200

    improved adaptive design

--- source/js/script.js
+++ source/js/script.js
@@ -1,10 +1,35 @@
 const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
-    loop: true,
-  
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-  });
+  direction: 'horizontal',
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
+
+const toggler = document.querySelector('.header__toggler');
+const nav = document.querySelector('.header__wrapper');
+const user = document.querySelector('.header__user');
+
+toggler.addEventListener('click', function () {
+  if (toggler.classList.contains('header__toggler--open')) {
+    toggler.classList.remove('header__toggler--open');
+    toggler.classList.add('header__toggler--close');
+
+    nav.classList.remove('header__wrapper--open');
+    nav.classList.add('header__wrapper--close');
+  }
+  else {
+    toggler.classList.add('header__toggler--open');
+    toggler.classList.remove('header__toggler--close');
+
+    nav.classList.add('header__wrapper--open');
+    nav.classList.remove('header__wrapper--close');
+  }
+});
+
+
+
